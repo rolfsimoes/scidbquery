e60da6228b14e62fd8b52fc350e72979ab3631c8
Rolf Simoes <rolfsimoes@gmail.com>
2017-10-16 19:49:57 -0200

    introduces ssh in scidb.exec

--- R/scidb.R
+++ R/scidb.R
@@ -18,11 +18,16 @@ utils::globalVariables(c(".data"))
 #' @description  Execute AFL instruction
 #' @param afl          A valid AFL scidb query
 #' @param fetch_data   Logical. Inform if data must be retrieved from \code{afl} query (Default \code{TRUE}).
+#' @param ssh_server   String informing a ssh conection, i.e. 'user@server', to run iquery.
 #' @return Tibble with AFL result
 #' @export
-scidb.exec <- function(afl, fetch_data = TRUE){
+scidb.exec <- function(afl, fetch_data = TRUE, ssh_server = NULL){
     if (fetch_data){
-        result <- system(sprintf("iquery -aq \"%s;\"", afl), intern = TRUE)
+        if (!is.null(ssh_server))
+            result <- system(sprintf("ssh s <<'ENDSSH'\n%s\nENDSSH"), 
+                             sprintf("iquery -aq \"%s;\"", afl), intern = TRUE)
+        else
+            result <- system(sprintf("iquery -aq \"%s;\"", afl), intern = TRUE)
         if (length(result) > 1){
             result <- result[1:(length(result)-1)]
             result <- gsub("[{}]", "", result)
@@ -31,7 +36,11 @@ scidb.exec <- function(afl, fetch_data = TRUE){
                 tibble::as.tibble()
         }
     } else {
-        result <- system(sprintf("iquery -aqn \"%s;\"", afl), intern = TRUE)
+        if (!is.null(ssh_server))
+            result <- system(sprintf("ssh s <<'ENDSSH'\n%s\nENDSSH"), 
+                             sprintf("iquery -aqn \"%s;\"", afl), intern = TRUE)
+        else
+            result <- system(sprintf("iquery -aqn \"%s;\"", afl), intern = TRUE)
     }
     return(result)
 }
